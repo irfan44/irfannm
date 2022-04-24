a17a1cac4f7392f94cb5a6af43e7cb2e7b6e636c
irfan44 <irfannmuhajir@gmail.com>
2022-04-24 00:57:55 +0000

    feat: show underline if nav menu active

--- components/layouts/Header/Menu/index.tsx
+++ components/layouts/Header/Menu/index.tsx
@@ -1,12 +1,20 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import menuList from 'data/menuList';
 
 const Menu = () => {
+  const router = useRouter();
+
   return (
     <>
       {menuList.map(({ name, link }) => {
         return (
-          <li key={name} className="hover:underline">
+          <li
+            key={name}
+            className={`hover:underline ${
+              router.asPath == link ? 'underline' : ''
+            }`}
+          >
             <Link href={link}>
               <a>{name}</a>
             </Link>
