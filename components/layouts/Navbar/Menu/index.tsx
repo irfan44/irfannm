dc4b0831fc005ccf848da7d6b51a9a87ed74e293
irfan44 <irfannmuhajir@gmail.com>
2022-04-24 03:15:53 +0000

    refactor: refactor navbar

--- components/layouts/Navbar/Menu/index.tsx
+++ components/layouts/Navbar/Menu/index.tsx
@@ -1,10 +1,7 @@
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import menuList from 'data/menuList';
 
 const Menu = () => {
-  const router = useRouter();
-
   return (
     <>
       {menuList.map(({ name, link }) => {
