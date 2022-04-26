002d8659f571c82a12d8a9956f2e3895c53e5bce
irfan44 <irfannmuhajir@gmail.com>
2022-04-26 22:40:24 +0700

    fix: use grid to show projects

--- components/templates/Projects/ProjectCard/index.tsx
+++ components/templates/Projects/ProjectCard/index.tsx
@@ -7,31 +7,38 @@ type Props = {
   techStack: string[];
   sourceCode: string;
 };
+
 const ProjectCard = ({ name, description, techStack, sourceCode }: Props) => {
   return (
-    <div className="space-y-3 border border-dark dark:border-neutral-50 rounded-2xl p-5 hover:shadow">
-      <h4>{name}</h4>
-      <p>{description}</p>
-      <div className="flex space-x-2">
-        {techStack.map((stack) => {
-          return (
-            <div
-              key={stack}
-              className="bg-dark text-white dark:bg-transparent dark:border dark:border-neutral-50 px-2 py-1 rounded-lg text-sm"
-            >
-              <p>{stack}</p>
-            </div>
-          );
-        })}
+    <div className="flex flex-col justify-between space-y-3 border border-dark dark:border-neutral-50 rounded-2xl p-5 hover:shadow">
+      <div>
+        <h4>{name}</h4>
       </div>
-      {sourceCode.length !== 0 && (
-        <div className="flex space-x-2 items-center hover:underline">
-          <IconsGithub />
-          <Link href={sourceCode}>
-            <a>Source Code</a>
-          </Link>
+      <div>
+        <p>{description}</p>
+      </div>
+      <div className="space-y-4">
+        <div className="flex space-x-2">
+          {techStack.map((stack) => {
+            return (
+              <div
+                key={stack}
+                className="bg-dark text-white dark:bg-transparent dark:border dark:border-neutral-50 px-2 py-1 rounded-lg text-sm flex items-center"
+              >
+                <p>{stack}</p>
+              </div>
+            );
+          })}
         </div>
-      )}
+        {sourceCode.length !== 0 && (
+          <div className="flex space-x-2 items-center hover:underline">
+            <IconsGithub />
+            <Link href={sourceCode}>
+              <a>Source Code</a>
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
