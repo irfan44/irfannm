fb2c49e780e5f71632bcc5b13b4c1732b0e97dff
Irfan Nurghiffari Muhajir <49281347+irfan44@users.noreply.github.com>
2022-05-14 18:01:24 +0700

    fix: remove unused import

--- components/layouts/Navbar/Menu/index.tsx
+++ components/layouts/Navbar/Menu/index.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import menuList from 'data/menuList';
-import ThemeSwitcher from 'components/common/ThemeSwitcher';
 
 const Menu = () => {
   const route = useRouter();
