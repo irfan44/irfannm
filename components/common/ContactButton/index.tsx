aa375efd8af32ec1cc27fce4f25a6fdf5e5a4f69
irfan44 <irfannmuhajir@gmail.com>
2022-05-15 04:07:37 +0000

    fix: add shadow to increase visibility

--- components/common/ContactButton/index.tsx
+++ components/common/ContactButton/index.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const ContactButton = () => {
   return (
-    <div className="w-fit bg-neutral-200 py-2 px-3 rounded-2xl text-black dark:bg-neutral-800 dark:text-white hover:bg-neutral-100 dark:hover:bg-neutral-700">
+    <div className="w-fit bg-neutral-100 py-2 px-3 rounded-2xl text-black dark:bg-neutral-800 dark:text-white hover:bg-neutral-100 dark:hover:bg-neutral-700 shadow-sm">
       <Link href="mailto:irfannmuhajir@gmail.com">
         <a target="_blank" rel="noopener noreferrer">
           <div className="flex items-center space-x-2">
--- components/common/ThemeSwitcher/index.tsx
+++ components/common/ThemeSwitcher/index.tsx
@@ -128,8 +128,8 @@ const ThemeSwitcher = () => {
       checked={currentTheme}
       onChange={setCurrentTheme}
       className={`${
-        currentTheme === 'dark' ? 'bg-neutral-800' : 'bg-neutral-200'
-      } relative inline-flex h-8 w-16 items-center rounded-full`}
+        currentTheme === 'dark' ? 'bg-neutral-800' : 'bg-neutral-100'
+      } relative inline-flex h-8 w-16 items-center rounded-full shadow-sm`}
     >
       <span className="sr-only">Enable notifications</span>
       <span
