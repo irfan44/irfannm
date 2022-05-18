41cf739d5840a8a0f37a6177b8eeb4410f8b8f13
irfan44 <irfannmuhajir@gmail.com>
2022-05-18 01:41:13 +0000

    fix: update social links

--- components/common/SocialLinks/index.tsx
+++ components/common/SocialLinks/index.tsx
@@ -1,29 +1,19 @@
 import Link from 'next/link';
-import { FaGithub, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
+import socialList from 'data/socialList';
 
 const SocialLinks = () => {
   return (
-    <div className="flex space-x-4 text-2xl text-black dark:text-white">
-      <div>
-        <Link href="https://github.com/irfan44" passHref>
-          <a target="_blank" rel="noopener noreferrer" title="Github">
-            <FaGithub />
-          </a>
-        </Link>
-      </div>
-      <div>
-        <Link href="https://www.linkedin.com/in/irfannm/" passHref>
-          <a target="_blank" rel="noopener noreferrer" title="LinkedIn">
-            <FaLinkedinIn />
-          </a>
-        </Link>
-      </div>
-      <div>
-        <Link href="https://www.instagram.com/irfan_nm44/" passHref>
-          <a target="_blank" rel="noopener noreferrer" title="Instagram">
-            <FaInstagram />
-          </a>
-        </Link>
+    <div className="text-2xl text-black dark:text-white">
+      <div className="flex space-x-4">
+        {socialList.map((social) => {
+          return (
+            <Link key={social.name} href={social.link} passHref>
+              <a target="_blank" rel="noopener noreferrer" title={social.name}>
+                <social.icon />
+              </a>
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
