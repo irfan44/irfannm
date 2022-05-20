3897bc1c38e5c49d001e9872f183462dbd98e2fa
Irfan Nurghiffari Muhajir <49281347+irfan44@users.noreply.github.com>
2022-05-20 23:30:48 +0000

    feat: add page routing animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import { ThemeProvider } from 'next-themes';
 import { AppProps } from 'next/app';
 import 'styles/index.css';
+import Router from 'next/router';
+
+import NProgress from 'nprogress'; //nprogress module
+import 'styles/nprogress.css'; //styles of nprogress\
+
+NProgress.configure({ showSpinner: false });
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
