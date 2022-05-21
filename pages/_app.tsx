1bb5cfb528859b2a3d26059e97bdaf43b28e7173
Irfan Nurghiffari Muhajir <49281347+irfan44@users.noreply.github.com>
2022-05-21 05:58:48 +0000

    fix: make route remember scroll position

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,16 @@ import Router from 'next/router';
 
 import NProgress from 'nprogress'; //nprogress module
 import 'styles/nprogress.css'; //styles of nprogress\
+import { useScrollRestoration } from 'utils/useScrollRestoration';
 
 NProgress.configure({ showSpinner: false });
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps, router }: AppProps) {
+  useScrollRestoration(router);
+  
   return (
     <ThemeProvider enableSystem={true} attribute="class">
       <Component {...pageProps} />
