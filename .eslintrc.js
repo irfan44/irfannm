03907228ad64bcfd37993331f14e6ea166a307ed
Irfan Nurghiffari Muhajir <49281347+irfan44@users.noreply.github.com>
2023-05-23 06:25:37 +0000

    chore: update packages & remove pwa

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,3 @@
 module.exports = {
-  extends: ['next', 'next/core-web-vitals', 'prettier'],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  rules: {
-    'import/order': [
-      'error',
-      {
-        groups: ['builtin', 'external', 'internal'],
-        alphabetize: { order: 'asc', caseInsensitive: true },
-      },
-    ],
-    'react/jsx-max-depth': [2, { max: 4 }],
-  },
+  extends: ['next', 'next/core-web-vitals'],
 };
