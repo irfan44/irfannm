6e388d278d3b3baff53891e05bcb375a428b1e46
irfan44 <irfannmuhajir@gmail.com>
2023-06-02 16:24:54 +0700

    fix: fix read more styling & change sitemap using next-sitemap

--- components/home/ReadMore.tsx
+++ components/home/ReadMore.tsx
@@ -4,7 +4,7 @@ import { HiArrowNarrowRight } from 'react-icons/hi';
 const ReadMore = () => {
   return (
     <Link href="/blog" passHref>
-      <p className="mt-4 font-medium flex items-center hover:cursor-pointer text-black dark:text-white">
+      <p className="mt-4 font-medium flex items-center hover:cursor-pointer hover:underline text-black dark:text-white">
         Read all posts
         <HiArrowNarrowRight className="text-xl ml-1" />
       </p>
