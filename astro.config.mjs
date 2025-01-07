7e8714317bcc8513dd07bed20dcf5ab01d826a09
irfan44 <irfannmuhajir@gmail.com>
2025-01-08 05:21:58 +0700

    feat: add redirect for old blog slug

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,26 @@ import sitemap from '@astrojs/sitemap'
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), sitemap()],
+  redirects: {
+    '/blog/2022-04-10-generasi-gigih-critical-thinking': {
+      status: 301,
+      destination: '/blog/generasi-gigih-soft-skill-critical-thinking',
+    },
+    '/blog/2022-04-01-menyambut-ramadan-dengan-berinvestasi-syariah': {
+      status: 301,
+      destination: '/blog/menyambut-ramadan-dengan-berinvestasi-syariah',
+    }, 
+    '/blog/2020-08-08-perancangan-arsitektur-perangkat-lunak': {
+      status: 301,
+      destination: '/blog/perancangan-arsitektur-perangkat-lunak',
+    },
+    '/blog/2020-08-11-kebutuhan-fungsional-dan-nonfungsional': {
+      status: 301,
+      destination: '/blog/kebutuhan-fungsional-dan-nonfungsional',
+    },
+    '/blog/2020-07-21-perbedaan-bahasa-pemrograman': {
+      status: 301,
+      destination: '/blog/perbandingan-bahasa-pemrograman-cpp-java-dan-c',
+    }
+  }
 })
