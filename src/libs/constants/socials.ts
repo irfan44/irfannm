adc137cf8ca7d78ecf8e5bcc302ed7eae2da731e
irfan44 <irfannmuhajir@gmail.com>
2025-01-11 11:44:39 +0700

    fix: migrate social media to astro icon

--- src/libs/constants/socials.ts
+++ src/libs/constants/socials.ts
@@ -1,32 +1,29 @@
-import { FaEnvelope, FaGithub, FaInstagram, FaLinkedinIn } from 'react-icons/fa'
-import type { IconType } from 'react-icons'
-
 interface SocialMedia {
   name: string
   link: string
-  icon: IconType
+  icon: string
 }
 
 const SOCIALS: SocialMedia[] = [
   {
     name: 'Email',
     link: 'mailto:irfannmuhajir@gmail.com',
-    icon: FaEnvelope,
+    icon: 'envelope',
   },
   {
     name: 'Github',
     link: 'https://github.com/irfan44',
-    icon: FaGithub,
+    icon: 'github',
   },
   {
     name: 'Linkedin',
     link: 'https://www.linkedin.com/in/irfannm',
-    icon: FaLinkedinIn,
+    icon: 'linkedin',
   },
   {
     name: 'Instagram',
     link: 'https://www.instagram.com/irfannm44',
-    icon: FaInstagram,
+    icon: 'instagram',
   },
 ]
 
